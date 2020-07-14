cbbf7a004fc7ff9189f740eef179224cb6920be8
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-07-14 14:35:31 -0400

    tried to put app.use in the middleware function

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,49 +1,57 @@
-const router = require("express").Router();
+var express = require("express");
+var router = express.Router();
 const Workout = require("../models/workout.js");
 
-router.post("/api/workouts", ({ body }, res) => {
-  Workout.create(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
+module.exports = function(app) {
+    router.use(function timeLog (req, res, next) {
+        console.log("Time: ", Date.now());
+        next();
     });
-});
-
-router.get("/api/workouts", (req, res) => {
-  Workout.find({})
-    .sort({ date: -1 })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
-router.push("/api/workouts/:id", ({ body, params }, res) => {
-    Workout.findByIdAndUpdate(
-        params.id,
-        { $push: {exercises: body }},
-
-        { new: true, runValidators: true }
-    ).then (dbWorkout => {
-        res.json(dbWorkout);
-    }).catch (err => {
-        res.json(err);
-    })
-});
-
-router.get("/api/workouts/range", (req,res) => {
-    Workout.find({})
-        .then(dbWorkout => {
-            //console.log(dbWorkout)
+    
+    router.post("/api/workouts", ({ body }, res) => {
+        Workout.create(body)
+          .then(dbWorkout => {
             res.json(dbWorkout);
+          })
+          .catch(err => {
+            res.status(400).json(err);
+          });
+      });
+      
+    router.get("/api/workouts", (req, res) => {
+        Workout.find({})
+          .sort({ date: -1 })
+          .then(dbWorkout => {
+            res.json(dbWorkout);
+          })
+          .catch(err => {
+            res.status(400).json(err);
+          });
+    });
+      
+    router.push("/api/workouts/:id", ({ body, params }, res) => {
+        Workout.findByIdAndUpdate(
+            params.id,
+            { $push: {exercises: body }},
+      
+            { new: true, runValidators: true }
+        ).then (dbWorkout => {
+              res.json(dbWorkout);
+        }).catch (err => {
+              res.json(err);
         })
-        .catch(err => {
-            res.json(err);
-        })
-});
+    });
+      
+    router.get("/api/workouts/range", (req,res) => {
+        Workout.find({})
+            .then(dbWorkout => {
+                //console.log(dbWorkout)
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            })
+    });
+}
 
-module.exports = router;
+
