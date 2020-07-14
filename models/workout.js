3fe184289541f98f5ed6d0bb105a228bbbb11bb3
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-07-14 13:46:03 -0400

    modified workoutjs

--- models/workout.js
+++ models/workout.js
@@ -3,13 +3,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    date: {
+    day: {
         type: Date,
         default: Date.now
     },
     exercise: [{
-        type: Schema.Types.ObjectId,
-        ref: "exercise"
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter your exercise type"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter your exercise name"
+        },
+        duration: {
+            type: Number,
+            required: "Enter your exercise duration"
+        },
+        distance: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        }       
     }]
 });
 
